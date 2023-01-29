70d6750f4934830ff6e4b24fa7be65ad7b9b8d09
Covadonga M <cova.millan@gmail.com>
2023-01-29 18:59:58 +0100

    fixed coverage errors

--- js/board.funct.test.js
+++ js/board.funct.test.js
@@ -9,5 +9,14 @@ describe('Given doBoard function', () => {
       const received = board.length;
       expect(received).toBe(expected.length);
     });
+
+    test('Then the function pushes random 0s and 1ns and returns a 6x6 board', () => {
+      const rows = 10;
+      const columns = 10;
+      const board = [0, 1, 0, 1, 1, 1, 0, 1, 0, 1];
+      const expected = doBoard(rows, columns);
+      const received = board.length;
+      expect(received).toBe(expected.length);
+    });
   });
 });
