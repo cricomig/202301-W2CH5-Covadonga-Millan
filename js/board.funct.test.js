f47d30333c1df311ab11b3ffffe352686664e771
Covadonga M <cova.millan@gmail.com>
2023-01-29 19:04:54 +0100

    fixed more errors

--- js/board.funct.test.js
+++ js/board.funct.test.js
@@ -10,10 +10,19 @@ describe('Given doBoard function', () => {
       expect(received).toBe(expected.length);
     });
 
-    test('Then the function pushes random 0s and 1ns and returns a 6x6 board', () => {
+    test('Then the function pushes random 0s and 1ns and returns a 10x10 board', () => {
       const rows = 10;
       const columns = 10;
       const board = [0, 1, 0, 1, 1, 1, 0, 1, 0, 1];
+      const expected = doBoard(rows, columns);
+      const received = board.length;
+      expect(received).toBe(expected.length);
+    });
+
+    test('Then the function pushes random 0s and 1ns and returns an empty board', () => {
+      const rows = 0;
+      const columns = 0;
+      const board = [];
       const expected = doBoard(rows, columns);
       const received = board.length;
       expect(received).toBe(expected.length);
